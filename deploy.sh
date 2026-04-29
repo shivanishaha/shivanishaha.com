caf4003e3c7df88698ba234952c6974dd15c460e
Amay Kadakia <kadakiaamay02@gmail.com>
2026-04-28 20:15:06 -0500

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -21,7 +21,7 @@ echo "Deployment to shivanishaha.com successful at $(date)"
 NOTIFY_EMAIL="amaykadakia@gmail.com,shivani24.shaha@gmail.com"
 
 DEPLOY_TIME=$(date)
-echo "Deployment to shivanishaha.com was successful at $DEPLOY_TIME." \
+echo "Deployment to shivanishaha.com was successful at $DEPLOY_TIME. This is an automated notification" \
   | mail -s "✅ Deploy Successful – shivanishaha.com" $NOTIFY_EMAIL
 
 echo "Deployment to shiavanishaha.com successful and API activated at $DEPLOY_TIME"
